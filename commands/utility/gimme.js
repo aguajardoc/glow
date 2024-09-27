5d091b410474e71e64608e7d326fede34bfbb1dd
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2024-09-27 12:26:41 -0600

    Refactored gimme, started gitgud, new commands

--- commands/utility/gimme.js
+++ commands/utility/gimme.js
@@ -2,10 +2,34 @@ const { fetchContest } = require('../../command_helpers/problem-fetcher');
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 
+async function gimmeInteraction(difficulty) {
+	const problemData = await fetchContest(difficulty);
+
+	// Create embed based on found problem.
+	const problemEmbed = {
+		color: 0x1e1e22,
+		title: `${problemData.chosenProblemLetter}. ${problemData.chosenProblemName}`,
+		url: `https://codeforces.com/gym/${problemData.contestId}/problem/${problemData.chosenProblemLetter}`,
+		description: problemData.contestName,
+		fields: [
+			{
+				name: 'Rating',
+				value: difficulty,
+			},
+		]
+	}
+
+	// Send it.
+	await interaction.editReply({
+		content: `Challenge problem for ${interaction.user.username}`,
+		embeds: [problemEmbed]
+	});
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gimme')
-		.setDescription('Provides a random problem from an Official ICPC contest.')
+		.setDescription('Provides a random problem from an Official ICPC contest, based on chosen difficulty.')
 		.addIntegerOption(option =>
 			option
 				.setName('difficulty')
@@ -16,28 +40,10 @@ module.exports = {
 		),
 	async execute(interaction) {
 		await interaction.deferReply();
-		// Find a problem based on difficulty.
+		// Find and send a problem based on difficulty.
 		const difficulty = interaction.options.getInteger('difficulty');
-		const problemData = await fetchContest(difficulty);
-
-		const problemEmbed = {
-			color: 0x1e1e22,
-			title: `${problemData.chosenProblemLetter}. ${problemData.chosenProblemName}`,
-			url: `https://codeforces.com/gym/${problemData.contestId}/problem/${problemData.chosenProblemLetter}`,
-			description: problemData.contestName,
-			fields: [
-				{
-					name: 'Rating',
-					value: difficulty,
-				},
-			]
-		}
-
-		// Send it
-		await interaction.editReply({
-			content: `Challenge problem for ${interaction.user.username}`,
-			embeds: [problemEmbed]
-		});
-		
+		gimmeInteraction(difficulty);
 	},
 };
+
+module.exports = { gimmeInteraction };
---
+++ commands/utility/ladder.js
@@ -0,0 +1,3 @@
+// A different mode of training: allows a user to train progressively harder problems starting from a set difficulty.
+// For example, a user can start at difficulty 3, solve it, and go up to 4,and so on.
+// Keep statistics like time taken, problems solved, and reached difficulty for self-comparison and leaderboards.
