f49cd60a1da88738352c552bf09d820651996b67
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2024-09-30 19:33:12 -0600

    Changed error embed to be more friendly

--- command_helpers/api-error.js
+++ command_helpers/api-error.js
@@ -2,7 +2,8 @@ async function errorMessage(interaction) {
     // Build an embed to inform user about an error with Codeforces's API.
     const errorEmbed = {
         color: 0xffbf00,
-        description: 'Codeforces API error.'
+        title: 'Codeforces API error',
+        description: 'This is neither your fault nor ours. Please wait some time, then try again.'
     }
     
     // Send this embed.
