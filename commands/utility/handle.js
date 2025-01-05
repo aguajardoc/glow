42dc2ecfe9e131983eef03547da693ad7f07bebb
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2025-01-05 08:09:12 -0600

    Added handle verification

--- commands/utility/handle.js
+++ commands/utility/handle.js
@@ -3,6 +3,23 @@ const { SlashCommandBuilder } = require("discord.js");
 const { errorMessage } = require('../../command_helpers/api-error');
 const { fetchContest } = require("../../command_helpers/problem-fetcher");
 
+async function verifyHandle(codeforcesHandle) {
+	userURL = `https://codeforces.com/api/user.info?handles=${codeforcesHandle}`;
+
+	try {
+		const response = await fetch(userURL);
+		if (!response.ok) {
+			return 0;
+		}
+		return 1;
+		
+	} 
+	catch (error) {
+		console.error('Error:', error);
+		return -1;
+	}
+}
+
 async function identify(codeforcesHandle) {
     let identificationEmbed = {
 		color: 0x1e1e22
@@ -13,36 +30,51 @@ async function identify(codeforcesHandle) {
         
         identificationEmbed.description = `${interaction.user.username}, you cannot identify when your handle is already set. Ask an Admin or Moderator if you wish to change it.`;
 	    });
-
+	*/
 		// Else, link the Discord user to their Codeforces handle.
 			// If their handle is not found in Codeforces, send the following message:
-			identificationEmbed.description = `Codeforces handle for ${interaction.user.username} not found in database. Use ;handle identify <cfhandle> (where <cfhandle> needs to be replaced with your codeforces handle, e.g. ;handle identify tourist) to add yourself to the database`;
-	    	});
+	const handleExists = await verifyHandle(codeforcesHandle);
 
-			// Else, ask for their submission of a compilation error to a difficulty 9 or 10 problem.
-			// This range of difficulties ensures there are no collisions in the submissions.
-			// This is the way the bot will know that the user has access to their account.
-    */
-
-	// Codeforces rank colors, in ascending order of the rank they represent:
-	// (Newbie, Pupil, Specialist, Expert, Candidate Master, 
-	// {Master, International Master}, 
-	// {Grandmaster, International Grandmaster, Legendary Grandmaster})
-	const rankColors = [0x808080, 0x008000, 0x03a89e, 0x0000ff, 0xaa00aa, 0xff8c00, 0xff0000];
-	
-	// Create embed based on the user's information.
-	identificationEmbed = {
-		color: 0x379c6f, // TODO: make this color congruent to their Codeforces rank.
-		title: `${problemData.chosenProblemLetter}. ${problemData.chosenProblemName}`,
-		url: `https://codeforces.com/${problemData.contestId < 100000 ?'contest' : 'gym'}/${problemData.contestId}/problem/${problemData.chosenProblemLetter}`,
-		description: `Handle for ${interaction.user.username} successfully set to (handle, with url to Codeforces)`,
+	if (handleExists === -1) {
+		errorMessage(interaction);
 	}
+	else if (handleExists === 0) {
+		identificationEmbed.description = 	`Codeforces handle for ${interaction.user.username} 
+											not found in database. Use ;handle identify 
+											<cfhandle> (where <cfhandle> needs to be replaced 
+											with your codeforces handle, e.g. ;handle identify 
+											tourist) to add yourself to the database`;
+	}
+	else if (handleExists === 1) {
 
-	// Send it.
-	await interaction.editReply({
-		content: `Challenge problem for ${interaction.user.username}`,
-		embeds: [problemEmbed]
-	});
+	// Else, ask for their submission of a compilation error to a difficulty 9 or 10 problem.
+	// This range of difficulties ensures there are no collisions in the submissions.
+	// This is the way the bot will know that the user has access to their account.
+
+    
+
+		// Codeforces rank colors, in ascending order of the rank they represent:
+		// (Newbie, Pupil, Specialist, Expert, Candidate Master, 
+		// {Master, International Master}, 
+		// {Grandmaster, International Grandmaster, Legendary Grandmaster})
+		const rankColors = [0x808080, 0x008000, 0x03a89e, 0x0000ff, 0xaa00aa, 0xff8c00, 0xff0000];
+
+		let userRank; // TODO : define this
+		
+		// Create embed based on the user's information.
+		identificationEmbed = {
+			color: rankColors[userRank], // TODO: make this color congruent to their Codeforces rank.
+			title: `${problemData.chosenProblemLetter}. ${problemData.chosenProblemName}`,
+			url: `https://codeforces.com/${problemData.contestId < 100000 ?'contest' : 'gym'}/${problemData.contestId}/problem/${problemData.chosenProblemLetter}`,
+			description: `Handle for ${interaction.user.username} successfully set to (handle, with url to Codeforces)`,
+		}
+
+		// Send it.
+		await interaction.editReply({
+			content: `Challenge problem for ${interaction.user.username}`,
+			embeds: [problemEmbed]
+		});
+	}
 }
 
 // A user is identified by running this command with their Codeforces handle as a parameter.
