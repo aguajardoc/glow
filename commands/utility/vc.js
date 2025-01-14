0d7e8691398bdeea7943b7f3d15559c3b1fa8c4e
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2025-01-14 07:16:48 -0600

    VC iteration

--- commands/utility/vc.js
+++ commands/utility/vc.js
@@ -8,6 +8,7 @@ const { fetchContest } = require("../../command_helpers/problem-fetcher");
 // Fetch the possible contest IDs.
 const possibleGyms = getArrayPossibleGymContests();
 const possibleNonGyms = getArrayPossibleNonGymContests();
+const totalLength = possibleGyms.length() + possibleNonGyms.length();
 
 // Reverse the arrays to get them in reverse chronological order.
 possibleGyms.reverse();
@@ -18,14 +19,25 @@ let indexGyms = 0;
 let indexNonGyms = 0;
 
 // A factor to "move" the indices in a proportional manner over both lists.
-const conversionFactor = possibleGyms / possibleNonGyms;
+const conversionFactor = possibleGyms.length() / possibleNonGyms.length();
 
 // Iterate the lists
-for ...
+for (const i = 0; i < totalLength; i++) {
+    let contest;
+    if (round(possibleNonGyms * conversionFactor) < i) {
+        contest = possibleNonGyms[indexNonGyms];
+        indexNonGyms++;
+    }
+    else {
+        contest = possibleGyms[indexGyms];
+        indexGyms++;
+    }
+
+    let contestURL;
     // Check contest, to see if user has made ANY submissions to it.
 
     // If not, edit the response to include it.
 
     // Else, continue iterating
-
+}
 // Allow the user to keep interacting with the response for the next 5 minutes, so that they choose their contest.
